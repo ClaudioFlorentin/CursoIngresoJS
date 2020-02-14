6a88a9bb7a8abbb17f5da14c40538509810eb70c
alumno <alumno@utnfra.com.ar>
2020-02-13 22:12:45 -0300

    if

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,6 +3,26 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+    switch(mesDelAño)
+ {
+    case "Enero":
+        alert("felices vacaciones");
+        break;
+    case "Marzo":
+        alert("Feliz cumple");
+        break;
+    case "Julio":   
+        alert("felices vacaciones");
+        break;
+    case "Diciembre":
+        alert("Felices fiestas");
+        break;
+    default:
+        alert("este mes no hay evento");
+        break;
+
+ }
+
 
 
 
